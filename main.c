5a894cc75e9159addacf71d3451c07f0da3f133c
Ariel Rosado Rivera <ariel_00720@yahoo.com>
2013-09-02 08:37:32 -0400

    Adds linked list dictionary reading which reads git config

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 #include "util.h"
 
 struct Node
@@ -10,41 +11,40 @@ struct Node
 	struct Node *next;
 };
 
-struct Node *list = NULL, **nextp = &list;
-char line[1024];
+struct Node *load(FILE *file) {
+	struct Node *list = NULL, **nextp = &list;
+	char buffer[1024];
 
-int main(int argc, char** argv) { 
-	printf("git-ticket working correctly.");
-	system("git config -l > in.list");
-	
-	FILE *ifp;
-	char mode = 'r';
-
-	ifp = fopen("in.list", &mode);
-
-	if (ifp == NULL) {
-		fprintf(stderr, "Can't open input file  in.list!\n");
-		exit(1);
-	}
-
-	while (fgets(line, sizeof line, ifp) != NULL) {
-			
+	while (fgets(buffer, sizeof buffer, file) != NULL) {
 		struct Node *node;
-		node = (Node *)malloc(sizeof(struct Node) + strlen(line) + 1);
-		node->key = strtok(strcpy((char*)(node+1), line), "=\r\n");
+
+		node = (Node*)malloc(sizeof(struct Node) + strlen(buffer) + 1);
+		node->key = strtok(strcpy((char*)(node+1), buffer), "=\r\n");
 		node->value = strtok(NULL, "\r\n");
 		node->next = NULL;
 		*nextp = node;
 		nextp = &node->next;
 	}
 
-	Node * item;
-
-	while ((item = list->next) != NULL) {
-		printf("%s is equal to %s\n", item->key, item->value);
-	}
+	return list;
+}
 
 
-	fclose(ifp);
+int main(int argc, char** argv) { 
+	printf("git-ticket working correctly.");
+	system("git config -l > in.list");
+	
+	FILE *file;
+	char mode = 'r';
+	struct Node *head, *node;
 
+	file = fopen("in.list", &mode);
+	assert(file != NULL);
+	head = load(file);
+	fclose(file);
+
+	for (node = head; node != NULL; node = node->next)
+			printf("key=>\"%s\", val=>\"%s\"\n", node->key, node->value);
+	
+	return 0;
 }
