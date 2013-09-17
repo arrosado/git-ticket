f417cd331387f592beb272abba96d2dc8a128e5e
Ariel Rosado Rivera <ariel_00720@yahoo.com>
2013-09-16 22:55:42 -0400

    Adds a method to start a git editor

--- main.c
+++ main.c
@@ -2,57 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-#include "config.h"
+#include "util.h"
 
 int main(int argc, char** argv) { 
 
-	git_initialize();
+	printf("Hello world!\n");
 
-	printf("\n");
+	char *temp = NULL;
+	char cmd[255] = "[subject]\n\n[body]\n\n[refs]\n\n";
 
-	if (git_getConfig("user.name"))
-		printf("Username is %s\n", git_getConfig("user.name"));
+	temp = (char*)malloc(sizeof(char) * strlen(cmd) + 1);
 
-	if (git_getConfig("user.email"))
-		printf("User email is %s\n", git_getConfig("user.email"));
+	strcpy(temp, cmd);
 
-	if (git_isInsideWorkTree())
-		printf("Is in working tree\n");	
+	printf("%s\n", inputWithEditor(temp));
 
-	printf("Available branches are :\n");
+	free(temp);
 
-	char **branches = git_branches();
-	
-	char *branch = NULL;
-	int i = 0;
-	while((branch = branches[i++]) != NULL)
-		printf("%s\n", branch);
-
-	printf("git dir is %s\n", git_dir());
-
-	printf("Ticketing system configurations: \n");
-
-	
-	GitTicketConfig *config = git_parseConfig(true);
-
-	printf("ticket.name: %s\n", config->name);
-	printf("ticket.repo: %s\n", config->repo);
-	printf("ticket.service: %s\n", config->service);
-	printf("ticket.ssl: %d\n", config->ssl);
-	printf("ticket.format.list: %s\n", config->format_list);
-	printf("ticket.format.show: %s\n", config->format_show);
-	printf("ticket.format.comment: %s\n", config->format_comment);
-	printf("ticket.github.token: %s\n", config->gtoken);
-	printf("ticket.bitbucket.token: %s\n", config->btoken);
-	printf("ticket.bitbucket.token-secret: %s\n", config->btoken_secret);
-	printf("ticket.redmine.url: %s\n", config->rurl);
-	printf("ticket.redmine.password: %s\n", config->rpassword);
-	printf("ticket.redmine.token: %s\n", config->rtoken);
-
-	printf("repo name : %s\n", git_guess_repo_name());
-	printf("service name : %s\n", git_guess_service());
-
-	git_dispose();
-	
 	return 0;
 }
--- util.c
+++ util.c
@@ -1,5 +1,104 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 #include "util.h"
-#include "config.h"
 
+char *getGitEditor() {
+	system("git var GIT_EDITOR > in.giteditor");
+
+	char read = 'r';
+	char *buffer = NULL;
+	FILE *file;
+
+	file = fopen("in.giteditor", &read);
+
+	assert(file != NULL);
+
+	buffer = (char*)malloc(sizeof(char) * 255);
+
+	assert(fgets(buffer, sizeof buffer, file) != NULL);
+
+	fclose(file);
+
+	return buffer;	
+}
+
+void writeTemplate(char *fileName, char *string) {
+
+	char write = 'w';
+	FILE *file;
+
+	file = fopen(fileName, &write);
+
+	assert(file != NULL);
+
+	fputs(string, file);
+
+	fflush(file);
+
+	fclose(file);
+
+	return;
+}
+
+char *readFile(char *fileName) {
+	char read = 'r';
+	char *buffer = NULL;
+	unsigned long lSize;
+	size_t result;
+	FILE *file;
+
+	file = fopen(fileName, &read);
+
+	assert(file != NULL);
+
+	// Obtain file size.
+	fseek(file, 0, SEEK_END);
+	lSize = ftell(file);
+	rewind(file);
+
+	buffer = (char*)malloc(sizeof(char)*lSize);
+
+	if (buffer == NULL) { fputs("Memory error", stderr); exit(2); }
+
+	result = fread(buffer, 1, lSize, file);
+
+	if (result != lSize) { fputs("Reading error", stderr); exit(3); }
+
+	fclose(file);
+
+	return buffer;
+}
+
+void callEditor(char *editor, char *fileName) {
+	char *cmd = NULL;
+
+	cmd = (char*)malloc(sizeof(char) * (strlen(editor) + strlen(fileName) + 1));
+
+	sprintf(cmd, "%s %s", editor, fileName);
+
+	system(cmd);
+
+	free(cmd);
+}
+
+char *inputWithEditor(char *string) {
+
+	char path[11] = "in.editing";
+	char *temp = NULL;
+	char *editor = NULL;	
+
+	temp = (char*)malloc(sizeof(char) *(strlen(path) + 1));
+
+	strcpy(temp, path);
+
+	editor = getGitEditor();
+
+	writeTemplate(temp, string);
+
+	callEditor(editor, temp);
+
+	free(editor);
+
+	return readFile(temp);
+}
